05368e3dbeafe84995c0d9ff2ce6032c835165d1
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2025-01-08 13:22:14 -0300

    Add project files.

---
+++ 01Sincrona/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("### Programacao Sincrona ###");
+
+int espera = 4000;
+
+Console.WriteLine("Tecle algo para iniciar");
+Console.ReadLine();
+
+RealizarTarefa(espera);
+
+Console.WriteLine($"\nTempo gasto {espera / 1000} segundos");
+Console.WriteLine("\n### Fim do processamento");
+
+Console.ReadKey();
+
+static void RealizarTarefa(int tempo)
+{
+    Console.WriteLine("\n>>> Iniciando a tarefa...");
+    //Task.Delay(TimeSpan.FromSeconds(tempo));
+    Thread.Sleep(tempo);
+    Console.WriteLine(">>>Tarefa concluida.");
+}
