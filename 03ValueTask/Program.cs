b38cff85ea1b6f5e78b9b3a73e8cf1769ef5ea63
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2025-01-12 15:02:18 -0300

    basico sobre ValueTask

---
+++ 03ValueTask/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("\nIniciando a operacao assincrona...");
+
+await MetodoSemRetornoAsync();
+
+Console.WriteLine("\nIniciando a operacao assincrona...");
+
+var result = await MetodoRetornoValorAsync(20);
+Console.WriteLine(result);
+
+Console.ReadKey();
+
+static async ValueTask MetodoSemRetornoAsync()
+{
+    Console.WriteLine("-Metodo que nao retorna valor");
+    await Task.Delay(2000);
+}
+
+static async ValueTask<int> MetodoRetornoValorAsync(int valor)
+{
+    Console.WriteLine("-Metodo que retorna valor");
+    await Task.Delay(2000);
+    return valor * 2;
+}
