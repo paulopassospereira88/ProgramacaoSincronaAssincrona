5772d625413f2d84ed32b202d5e277ca478b6442
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2025-01-15 13:08:33 -0300

    basico de exception na programacao assincrona

---
+++ 05TratamentoException1/Program.cs
@@ -0,0 +1,27 @@
+﻿TesteAsync t = new();
+t.ChamaTarefaAsync();
+
+Console.ReadKey();
+
+class TesteAsync
+{
+    public Task MinhaTarefaAsync()
+    {
+        return Task.Run(() => {
+            Task.Delay(2000);
+            throw new Exception("Minha Exception...");
+        });
+    }
+
+    public async void ChamaTarefaAsync()
+    {
+        try
+        {
+            await MinhaTarefaAsync();
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
