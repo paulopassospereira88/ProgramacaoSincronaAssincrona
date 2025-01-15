60570063f0ce408b80b2f3362a1094a6bb3a7b17
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2025-01-15 13:32:19 -0300

    multiplas exception async

---
+++ 06TratamentoException2/Program.cs
@@ -0,0 +1,35 @@
+﻿await LancaMultiplasExcecoesAsync();
+
+Console.ReadKey();
+
+static async Task LancaMultiplasExcecoesAsync()
+{
+    Task? tarefas = null;
+    try
+    {
+        var primeiraTask = Task.Run(() =>
+        {
+            Task.Delay(1000);
+            throw new IndexOutOfRangeException("IndexOutOfRangeException lancada explicitamente.");
+        });
+
+        var segundaTask = Task.Run(() =>
+        {
+            Task.Delay(1000);
+            throw new IndexOutOfRangeException("IndexOutOfRangeException lancada explicitamente.");
+        });
+
+        tarefas = Task.WhenAll(primeiraTask, segundaTask);
+        await tarefas;
+    }
+    catch
+    {
+        Console.WriteLine("Ocorreram as seguintes excecoes :-\n");
+        AggregateException TodasExceptions = tarefas.Exception;
+
+        foreach(var ex in TodasExceptions.InnerExceptions)
+        {
+            Console.WriteLine(ex.GetType().ToString());
+        }
+    }
+}
